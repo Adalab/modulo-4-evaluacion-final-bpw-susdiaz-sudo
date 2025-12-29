809b0f4b9bc38018596c84caf4814a5527978062
Sus.diaz <sus.diazse@gmail.com>
2025-12-29 11:27:04 +0100

    Retrieving data from database

--- src/index.js
+++ src/index.js
@@ -11,12 +11,35 @@ const mysql = require("mysql2/promise");
 app.use(cors());
 app.use(express.json({ limit: "25Mb" }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
 
 const dataConnection = {
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  database: process.env.MYSQL_DATABASE,
 };
+
+const createConnection = async () => {
+  const connection = await mysql.createConnection(dataConnection);
+  await connection.connect();
+  return connection;
+};
+
+app.get("/api/books", async (req, res) => {
+  try {
+    queryOneBook =
+      "SELECT * FROM books";
+    const connection = await createConnection();
+    const [books] = await connection.execute(queryOneBook);
+    await connection.end();
+    res.json({ books: books });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
