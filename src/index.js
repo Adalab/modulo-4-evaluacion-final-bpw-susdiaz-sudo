c808e5898e9728d927bd915fee42c5f0c2d923cb
Sus.diaz <sus.diazse@gmail.com>
2025-12-29 08:58:05 +0100

    Configure the repository

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json({ limit: "25Mb" }));
+
+const port = process.env.PORT || 3000;
+
+const dataConnection = {
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+};
